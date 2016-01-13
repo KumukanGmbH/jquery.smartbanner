907b4f661607f59fed4d6c59c9c03692556bcb3f
Alfonz Borsos <alfonz.borsos@gmail.com>
2016-01-13 11:57:25 +0100

    remove different closing cases

--- jquery.smartbanner.js
+++ jquery.smartbanner.js
@@ -179,10 +179,7 @@
                 $(this.pushSelector).animate({paddingTop: this.origHtmlMargin}, this.options.speedIn, 'swing', callback);
             } else {
                 if ($.support.transition) {
-                    if ( this.type !== 'android' )
-                      banner.css('top', -1 * this.bannerHeight * this.scale).removeClass('shown');
-                    else
-                      banner.css({display:'none'}).removeClass('shown');
+                    banner.css({display:'none'}).removeClass('shown');
                     var transitionCallback = function () {
                         $('html').removeClass('sb-animation');
                         if (callback) {
